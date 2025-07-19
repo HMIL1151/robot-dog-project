8c83ed3d469ed674de69f32405a41099dc798437
Harrison Milburn <harrison.milburn@tharsus.co.uk>
2025-07-19 22:54:11 +0100

    Now able to control all 4 legs
    need to update offsets and tibia/femur code mix up. Also need to fix buck converter issue - getting too hot. Maybe use a power supply?

--- Project Koda/src/main.cpp
+++ Project Koda/src/main.cpp
@@ -1,146 +1,91 @@
-
 #include <Arduino.h>
-#include <ESP32Servo.h>
-#include "GaitLib.h"
-#include "GaitLookupTable.h"
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+ 
+Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver(0x40);
 
-Servo servoD, servoA;
-int servoPin1 = 25;
-int servoPin2 = 26;
+int hipZeroDeg = 90;
+int femurZeroDeg = 90;
+int tibiaZeroDeg = 90;
+ 
+int minServoTicks = 30;
+int maxServoTicks = 575;
 
-// Path storage
-const int maxPathPoints = 150;
-Point curvePoints[maxPathPoints];
-ServoAngles pathAngles[maxPathPoints];
-int pathLength = 0;
-bool isAtStart = false;
+int hipPosDeg = 110;
+int femurPosDeg = 85;
+int tibiaPosDeg = 113;
 
+int hipServos[] = {0, 1, 2, 3};
+int hipServosAdjustment[] = {20, -5, 0, 0};
+
+int femurServos[] = {8, 9, 10, 11};
+int femurServosAdjustment[] = {0, 0, 0, 0};
+
+int tibiaServos[] = {4, 5, 6, 7};
+int tibiaServosAdjustment[] = {0, 0, 0, 0};
+
+int hipCommandDeg, 
+    femurCommandDeg, 
+    tibiaCommandDeg;
+ 
 void setup() {
-  servoD.attach(servoPin1);
-  servoA.attach(servoPin2);
+ 
   Serial.begin(115200);
-
-  // Nothing here, all curve setup is now in loop()
-}
-
-void setKinematicAngles(int angleA, int angleD) {
-  int servoAnglesA = angleA;
-  int servoAnglesD = angleD - 90;
-  servoD.write(servoAnglesD); // right (d)
-  servoA.write(servoAnglesA); // left (a)
-}
-
-void followPath() {
-  Serial.println("Following path with angles:");
-  for (int i = 0; i < pathLength; i++) {
-    setKinematicAngles(pathAngles[i].thetaA, pathAngles[i].thetaD);
-    if (isAtStart) 
-    {
-      delay(1000); // Wait at start position
-      isAtStart = false; // Only wait once at the start
-    }
-    Serial.print("Angle A: ");
-    Serial.print(pathAngles[i].thetaA);
-    Serial.print(", Angle D: ");
-    Serial.println(pathAngles[i].thetaD);
-    delay(25); // Adjust delay as needed
-  }
-  isAtStart = true; // Reset to wait at start for next path
+  servoDriver.begin();
+  servoDriver.setPWMFreq(50);
+ 
 }
 
 void loop() {
-  // 1. Vertical line
-  // Serial.println("Executing: Vertical Line");
-  // {
-  //   CurveParams params;
-  //   params.x0 = 50; // x position
-  //   params.y0 = -120; // y start
-  //   params.yc = -60;  // y end
-  //   int curveCount = generateCurvePoints(VERTICAL, params, curvePoints, maxPathPoints, +1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(200);
 
-  //   curveCount = generateCurvePoints(VERTICAL, params, curvePoints, maxPathPoints, -1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(1000);
-  // }
-
-  // // 2. Horizontal line
-  // Serial.println("Executing: Horizontal Line");
-  // {
-  //   CurveParams params;
-  //   params.x0 = 20; // x start
-  //   params.xc = 80; // x end
-  //   params.y0 = -100; // y position
-  //   int curveCount = generateCurvePoints(HORIZONTAL, params, curvePoints, maxPathPoints, +1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(200);
-
-  //   curveCount = generateCurvePoints(HORIZONTAL, params, curvePoints, maxPathPoints, -1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(1000);
-  // }
-
-  // // 3. Straight line (y = m*x + c)
-  // Serial.println("Executing: Straight Line");
-  // {
-  //   CurveParams params;
-  //   params.x0 = 20; // x start
-  //   params.xc = 80; // x end
-  //   params.m = 0.5; // slope
-  //   params.c = -120; // intercept
-  //   int curveCount = generateCurvePoints(LINE, params, curvePoints, maxPathPoints, +1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(200);
-
-  //   curveCount = generateCurvePoints(LINE, params, curvePoints, maxPathPoints, -1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(1000);
-  // }
-
-  // // 4. Ellipse
-  // Serial.println("Executing: Ellipse");
-  // {
-  //   CurveParams params;
-  //   params.xc = 50; // center x
-  //   params.yc = -100; // center y
-  //   params.a = 30; // radius x
-  //   params.b = 20; // radius y
-  //   int curveCount = generateCurvePoints(ELLIPSE, params, curvePoints, maxPathPoints, +1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(200);
-
-  //   curveCount = generateCurvePoints(ELLIPSE, params, curvePoints, maxPathPoints, -1);
-  //   pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-  //   followPath();
-  //   delay(1000);
-  // }
-
-  // 5. Half-flat-ellipse
-  Serial.println("Executing: Half-Flat Ellipse");
-  {
-    CurveParams params;
-    params.xc = 23;
-    params.yc = -120;
-    params.a = 30;
-    params.b = 40;
-    int curveCount = generateCurvePoints(HALF_FLAT_ELLIPSE, params, curvePoints, maxPathPoints, +1);
-    pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-    followPath();
-    delay(200);
-
-    curveCount = generateCurvePoints(HALF_FLAT_ELLIPSE, params, curvePoints, maxPathPoints, -1);
-    pathLength = matchCurveToLookup(curvePoints, curveCount, lookupTable, lookupCount, pathAngles, maxPathPoints);
-    followPath();
-    delay(1000);
+  int hipDeltaDeg = hipPosDeg - hipZeroDeg;
+  int femurDeltaDeg = femurPosDeg - femurZeroDeg;
+  int tibiaDeltaDeg = tibiaPosDeg - tibiaZeroDeg;
+  
+  
+  for (int i = 0; i < sizeof(hipServos) / sizeof(hipServos[0]); i++) {
+    if (i == 0){
+      hipCommandDeg = hipZeroDeg + hipDeltaDeg;
+    }
+    else if (i == 1){
+      hipCommandDeg = hipZeroDeg - hipDeltaDeg;
+    }
+    else if (i == 2){
+      hipCommandDeg = hipZeroDeg + hipDeltaDeg;
+    }
+    else {
+      hipCommandDeg = hipZeroDeg - hipDeltaDeg;
+    }
+    int hipTicks = map(hipCommandDeg, 0, 180, minServoTicks, maxServoTicks);
+    int servoCommand = hipTicks + hipServosAdjustment[i];
+    servoDriver.setPWM(hipServos[i], 0, servoCommand);
   }
+
+  for (int i = 0; i < sizeof(femurServos) / sizeof(femurServos[0]); i++) {
+    if (i == 1 || i == 2){
+      femurCommandDeg = femurZeroDeg + femurDeltaDeg;
+    }
+    else {
+      femurCommandDeg = femurZeroDeg - femurDeltaDeg;
+    }
+
+    int femurTicks = map(femurCommandDeg, 0, 180, minServoTicks, maxServoTicks);
+    int servoCommand = femurTicks + femurServosAdjustment[i];
+    servoDriver.setPWM(femurServos[i], 0, servoCommand);
+  }
+
+  for (int i = 0; i < sizeof(tibiaServos) / sizeof(tibiaServos[0]); i++) {
+    if (i == 1 || i == 2){
+      tibiaCommandDeg = tibiaZeroDeg + tibiaDeltaDeg;
+    }
+    else {
+      tibiaCommandDeg = tibiaZeroDeg - tibiaDeltaDeg;
+    }
+    
+    int tibiaTicks = map(tibiaCommandDeg, 0, 180, minServoTicks, maxServoTicks);
+    int servoCommand = tibiaTicks + tibiaServosAdjustment[i];
+    servoDriver.setPWM(tibiaServos[i], 0, servoCommand);
+  }
+
+  delay(50);
 }
-
-
